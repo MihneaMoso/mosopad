4865421024e4b588d845deb46cbaccb740150374
𝖒𝖎𝖍𝖓𝖊𝖆 𝖒𝖔𝖘𝖔 <sons.mosorescu@gmail.com>
2025-11-25 22:46:58 +0200

    PLS fix deps

--- mosopad/vite.config.ts
+++ mosopad/vite.config.ts
@@ -2,25 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	 optimizeDeps: {
+    plugins: [sveltekit()],
+    optimizeDeps: {
         include: ['monaco-editor']
     },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    monaco: ['monaco-editor']
-                }
-            }
-        }
-    },
     server: {
-      fs: {
-        allow: [
-          searchForWorkspaceRoot(process.cwd()),
-          "../"
-        ]
-      }
+        fs: {
+            allow: [
+                searchForWorkspaceRoot(process.cwd()),
+                "../"
+            ]
+        }
     }
 });
